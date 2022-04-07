de5d5adbc3bc8106f4bead99d2e76b102cf1d176
Preston Fassbender <fassbenderp0551@gmail.com>
2022-04-06 19:48:57 -0500

    it wasn't the icon path

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
             fingerprints: false,
             inject: false,
             icons: [{
-                src: path.resolve("assets/icons/icon-512x512.png"),
+                src: path.resolve("assets/img/icons/icon-512x512.png"),
                 sizes: [96, 128, 192, 256, 384, 512],
                 destination: path.join("assets", "icons")
             }]
